f07d066847101b376842dc0bdc4edef1c0eb276e
Massingue2025 <antoniocarlosmassingue20@gmail.com>
2025-06-11 21:54:11 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import os
+import subprocess
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Chave e URL do Facebook Live
+STREAM_URL = 'rtmps://live-api-s.facebook.com:443/rtmp/'
+STREAM_KEY = 'FB-745433421335513-0-Ab2151bh5oex3yr_ADWG_rRV'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    video = request.files['video']
+    if video:
+        path = os.path.join(UPLOAD_FOLDER, video.filename)
+        video.save(path)
+
+        cmd = [
+            'ffmpeg',
+            '-re',
+            '-i', path,
+            '-c:v', 'libx264',
+            '-preset', 'veryfast',
+            '-maxrate', '3000k',
+            '-bufsize', '6000k',
+            '-pix_fmt', 'yuv420p',
+            '-g', '50',
+            '-c:a', 'aac',
+            '-b:a', '128k',
+            '-f', 'flv',
+            STREAM_URL + STREAM_KEY
+        ]
+        subprocess.Popen(cmd)
+
+        return "Live iniciada com sucesso!"
+    return "Erro ao enviar vídeo."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
